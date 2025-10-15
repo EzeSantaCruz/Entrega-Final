2b682299a1ffae4b24b00c4f5211a874103c13ff
EzeSantaCruz <ezequielfsc01@gmail.com>
2025-10-15 16:40:38 -0300

    agregue bodyparser

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from 'express'
 import {connection} from './db.js'
 import {PORT} from './config.js'
+import bodyParser from 'body-parser'
 
 const app = express()
 
 const puerto = PORT
 
 connection()
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+
 app.listen(puerto, () =>{
     console.log(`Corriendo el server en el puerto ${puerto}`)
 })
